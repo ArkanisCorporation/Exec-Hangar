49a19dbe5c9421427134029c309285630a0a940b
NBDBatman <xzombie256@gmail.com>
2025-10-28 08:30:36 +0000

    Update I guess

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-/**
- * PYAM Executive Hangar Status Tracker
- * 
- * MIT License
- * 
- * Copyright (c) 2025 Xyxyll
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- * @version 2.1.14
- * @description Tracks and displays PYAM hangar online/offline status for Star Citizen.
- * @lastModified 2025-Oct-26
- */
-
 const OPEN_DURATION = 3900496;
 const CLOSE_DURATION = 7200917;
 const CYCLE_DURATION = OPEN_DURATION + CLOSE_DURATION;
